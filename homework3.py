bc7951323df4180629863f99e400925bd70fce0d
BiteSmart <i@mmvbaks.ru>
2024-05-28 15:37:56 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,22 +1,57 @@
-TASK_STR_LEFT_INDENT = 3                # Indent for task strings with results
-task_str_first_column_width = 35        # Text width in task explanation
+# 2024-05-28
+### The homework code is below ###
 
-def e_print(message, result_value):     # Extended print with output format
-    print(f'{' ': >{TASK_STR_LEFT_INDENT}}{message: <{task_str_first_column_width}}{result_value}')
+name = 'Oleg'
+print(f'Hello! My name is {name}.')
+age = 40
+print(f'I\'m {age} years old.')
+age += 2
+print(f'Excuse me! I\'m {age} years old.')
+is_student = True
+print(f'My learning status is equal to {is_student}.')
 
-my_string = input('Input some text: ')
-if len(my_string) > 35:     # 35 is optimal for comments in task, but my_string length can be greater
-    task_str_first_column_width = len(my_string) + 5
+### End of the homework code ###
 
-print('\nTask 2. Organize the program')
-e_print('Text length by len() function is:', len(my_string))
-e_print('Text length by count method is:', my_string.count('') - 1)
-e_print('Text length by rfind method is:', my_string.rfind(''))
-e_print('Text length by rindex method is:', my_string.rindex(''))
 
-print('\nTask 3. Applying string methods.')
-e_print(my_string.upper(), 'This is upper case.')           # Выведите строку my_string в верхнем регистре.
-e_print(my_string.lower(), 'This is lower case.')           # Выведите строку my_string в нижнем регистре.
-e_print(my_string.replace(' ',''), 'Spaces removed.')       # Измените строку my_string, удалив все пробелы.
-e_print(my_string[0], 'First symbol here.')                 # Выведите первый символ строки my_string.
-e_print(my_string[-1], 'Last symbol here.')                 # Выведите последний символ строки my_string.
+"""
+=========================================================================================
+=================================== HOMEWORK CONTENTS ===================================
+=========================================================================================
+2024-05-28
+
+2023/09/23 00:00|Практическая работа по уроку "Динамическая типизация"
+Практическая работа по уроку "Динамическая типизация"
+
+Цель: Написать программу на языке Python, используя Pycharm, для демонстрации динамической типизации.
+
+    В проекте, где вы решаете домашние задания, создайте модуль 'homework3.py' и напишите весь код в нём.
+
+
+2. Создайте переменные разных типов данных:
+  - Создайте переменную name и присвойте ей значение вашего имени (строка).
+  - Выведите значение переменной name на экран.
+  - Создайте переменную age и присвойте ей значение вашего возраста (целое число).
+  - Выведите значение переменной age на экран.
+  - Перезапишите в age текущее значение переменной age + новое.
+Как неверно (просто перезапись на новое число):
+a = 15
+a = 17
+  - Выведите измененное значение переменной age на экран.
+  - Создайте переменную is_student и присвойте ей значение True (логическое значение).
+  - Выведите значение переменной is_student на экран.
+
+Примечания:
+- Для вывода значений на экран используйте функцию print().
+- Обратите внимание на использование разных типов данных и возможности их изменения.
+
+Пример результата выполнения программы:
+Name: John
+Age: 25
+New Age: 26
+Is Student: True
+
+Файл с кодом (homework3.py) прикрепите к домашнему заданию.
+
+Успехов!
+
+"""
